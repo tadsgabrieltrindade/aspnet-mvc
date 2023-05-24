3f6a514a3384204d0b28280d0e26ada6464b8f4c
Gabriel Trindade <icandoitgrt@gmail.com>
2023-05-24 07:41:26 -0300

    first commit

---
+++ FirstAPPInMVC/Controllers/HomeControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+
+namespace FirstAPPInMVC.Controllers
+{
+   [TestFixture]
+    public class HomeControllerTest
+    {
+
+        [Test]
+        public void TestIndexView()
+        {
+            // Arrange
+            int a = 5;
+            int b = 7;
+
+            // Act
+            int resultado = a + b;
+
+            // Assert
+            Assert.AreEqual(12, resultado);
+        }
+    }
+}
